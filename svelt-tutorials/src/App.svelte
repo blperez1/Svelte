9248f4178ce1637786ce251185ee2085882c789e
Unknown <blossomperez18@gmail.com>
2021-03-11 23:17:51 -0600

    Add image and styling to component

--- svelt-tutorials/src/App.svelte
+++ svelt-tutorials/src/App.svelte
@@ -1,8 +1,17 @@
 <script>
 	let name = 'World';
+	let src = './favicon.png'
 </script>
 
 <main>
+	<img {src} alt='svelte icon'>
 	<h1>Hello {name.toUpperCase()}!</h1>
 </main>
 
+<style>
+	h1 {
+		color: purple;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
