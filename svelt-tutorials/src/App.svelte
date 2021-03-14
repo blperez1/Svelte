6e137faaa0b1b3ab9ea33312c8d10de3f92a1747
Unknown <blossomperez18@gmail.com>
2021-03-13 23:57:05 -0500

    Using spread on component props

--- svelt-tutorials/src/App.svelte
+++ svelt-tutorials/src/App.svelte
@@ -1,7 +1,14 @@
-<!-- using props: must export variables from other components -->
 <script>
-	import Nested from './Nested.svelte'
+	import Info from './Info.svelte'
+
+	const pkg = {
+		name: 'svelte',
+		version: 3,
+		speed: 'blazing',
+		website: 'https://svelte.dev'
+	}
 </script>
 
-<Nested answer = {42}/>
-<Nested />
+<!-- next two lines will do the exact same thing. -->
+<Info {...pkg}/>
+<Info name={pkg.name} version={pkg.version} speed={pkg.speed} website={pkg.website} />
---
+++ svelt-tutorials/src/info.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let name;
+    export let version;
+    export let speed;
+    export let website;
+</script>
+
+<p>The <code>{name}</code> package is {speed} fast. Download {version} from <a href="https://www.npmjs.com/package/{name}">npm</a> and <a href={website}>Learn more here</a></p>
